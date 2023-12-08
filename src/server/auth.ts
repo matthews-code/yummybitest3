af54c4f0b501be7c3b902518624f1d65a4f41036
Matthew Buensalida <buensalidamatthew@gmail.com>
2023-12-08 21:37:39 +0800

    revert signin page

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -62,9 +62,9 @@ export const authOptions: NextAuthOptions = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
-  pages: {
-    signIn: "/auth/signin",
-  },
+  // pages: {
+  //   signIn: "/auth/signin",
+  // },
 };
 
 /**
