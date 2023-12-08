9fc91c0b5dda14e7d5653dac21c6f82f08414c89
Matthew Buensalida <buensalidamatthew@gmail.com>
2023-12-08 16:54:38 +0800

    move signin page to auth folder

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -4,7 +4,7 @@ import type {
 } from "next";
 import { getProviders, signIn } from "next-auth/react";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../server/auth";
+import { authOptions } from "../../server/auth";
 
 export default function SignIn({
   providers,
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -63,7 +63,7 @@ export const authOptions: NextAuthOptions = {
      */
   ],
   pages: {
-    signIn: "/signin",
+    signIn: "/auth/signin",
   },
 };
 
